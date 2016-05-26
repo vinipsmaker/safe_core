5857b5bb2f0c617b09318f6f6756181110b9518c
MaidSafe-QA <qa@maidsafe.net>
2016-05-26 15:46:38 +0000

    Updated documentation.

---
+++ 0.15.0/implementors/self_encryption/storage/trait.StorageError.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['safe_core'] = ["impl <a class='trait' href='self_encryption/storage/trait.StorageError.html' title='self_encryption::storage::StorageError'>StorageError</a> for <a class='struct' href='safe_core/core/self_encryption_storage_error/struct.SelfEncryptionStorageError.html' title='safe_core::core::self_encryption_storage_error::SelfEncryptionStorageError'>SelfEncryptionStorageError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ latest/implementors/self_encryption/storage/trait.Storage.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['safe_core'] = ["impl <a class='trait' href='self_encryption/storage/trait.Storage.html' title='self_encryption::storage::Storage'>Storage</a>&lt;<a class='struct' href='safe_core/core/self_encryption_storage_error/struct.SelfEncryptionStorageError.html' title='safe_core::core::self_encryption_storage_error::SelfEncryptionStorageError'>SelfEncryptionStorageError</a>&gt; for <a class='struct' href='safe_core/core/self_encryption_storage/struct.SelfEncryptionStorage.html' title='safe_core::core::self_encryption_storage::SelfEncryptionStorage'>SelfEncryptionStorage</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
