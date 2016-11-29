2706c8c8955204fdfed10261057fcb2b2bb0504f
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2016-11-29 14:07:30 -0300

    feat/auth: Implement Rust <-> FFI convertion functions

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -21,6 +21,7 @@
 
 use routing::XorName;
 use rust_sodium::crypto::{box_, secretbox, sign};
+use std::mem;
 
 /// TODO: doc
 pub mod ffi;
@@ -39,6 +40,48 @@ pub struct ContainerPermission {
     pub access: Vec<PermissionAccess>,
 }
 
+impl ContainerPermission {
+    /// TODO: doc
+    pub fn into_raw(self) -> *mut ffi::ContainerPermission {
+        let ContainerPermission { container_key, mut access } = self;
+
+        let ck_ptr = container_key.as_ptr();
+        let ck_len = container_key.len();
+        let ck_cap = container_key.capacity();
+
+        mem::forget(container_key);
+
+        let a_ptr = access.as_mut_ptr();
+        let a_len = access.len();
+        let a_cap = access.capacity();
+
+        mem::forget(access);
+
+        Box::into_raw(Box::new(ffi::ContainerPermission {
+            container_key: ck_ptr,
+            container_key_len: ck_len,
+            container_key_cap: ck_cap,
+            access: a_ptr,
+            access_len: a_len,
+            access_cap: a_cap,
+        }))
+    }
+
+    /// TODO: doc
+    #[allow(unsafe_code)]
+    pub unsafe fn from_raw(raw: *mut ffi::ContainerPermission) -> Self {
+        let raw = Box::from_raw(raw);
+        let ck = String::from_raw_parts(raw.container_key as *mut u8,
+                                        raw.container_key_len,
+                                        raw.container_key_cap);
+        let a = Vec::from_raw_parts(raw.access, raw.access_len, raw.access_cap);
+        ContainerPermission {
+            container_key: ck,
+            access: a,
+        }
+    }
+}
+
 /// TODO: doc
 pub struct AppExchangeInfo {
     /// TODO: doc
@@ -51,6 +94,73 @@ pub struct AppExchangeInfo {
     pub vendor: String,
 }
 
+impl AppExchangeInfo {
+    /// TODO: doc
+    pub fn into_raw(self) -> *mut ffi::AppExchangeInfo {
+        let AppExchangeInfo { id, scope, name, vendor } = self;
+
+        let id_ptr = id.as_ptr();
+        let id_len = id.len();
+        let id_cap = id.capacity();
+
+        mem::forget(id);
+
+        let (s_ptr, s_len, s_cap) = match scope {
+            Some(ref s) => (s.as_ptr(), s.len(), s.capacity()),
+            None => (0 as *const u8, 0, 0),
+        };
+
+        mem::forget(scope);
+
+        let n_ptr = name.as_ptr();
+        let n_len = name.len();
+        let n_cap = name.capacity();
+
+        mem::forget(name);
+
+        let v_ptr = vendor.as_ptr();
+        let v_len = vendor.len();
+        let v_cap = vendor.capacity();
+
+        mem::forget(vendor);
+
+        Box::into_raw(Box::new(ffi::AppExchangeInfo {
+            id: id_ptr,
+            id_len: id_len,
+            id_cap: id_cap,
+            scope: s_ptr,
+            scope_len: s_len,
+            scope_cap: s_cap,
+            name: n_ptr,
+            name_len: n_len,
+            name_cap: n_cap,
+            vendor: v_ptr,
+            vendor_len: v_len,
+            vendor_cap: v_cap,
+        }))
+    }
+
+    /// TODO: doc
+    #[allow(unsafe_code)]
+    pub unsafe fn from_raw(raw: *mut ffi::AppExchangeInfo) -> Self {
+        let raw = Box::from_raw(raw);
+        let id = String::from_raw_parts(raw.id as *mut u8, raw.id_len, raw.id_cap);
+        let scope = match (raw.scope, raw.scope_len, raw.scope_cap) {
+            (p, _, _) if p == 0 as *const u8 => None,
+            (p, l, c) => Some(String::from_raw_parts(p as *mut u8, l, c)),
+        };
+        let name = String::from_raw_parts(raw.name as *mut u8, raw.name_len, raw.name_cap);
+        let vendor = String::from_raw_parts(raw.vendor as *mut u8, raw.vendor_len, raw.vendor_cap);
+
+        AppExchangeInfo {
+            id: id,
+            scope: scope,
+            name: name,
+            vendor: vendor,
+        }
+    }
+}
+
 /// TODO: doc
 pub struct AuthRequest {
     /// TODO: doc
@@ -59,6 +169,48 @@ pub struct AuthRequest {
     pub app_container: bool,
     /// TODO: doc
     pub containers: Vec<ContainerPermission>,
+}
+
+impl AuthRequest {
+    /// TODO: doc
+    pub fn into_raw(self) -> *mut ffi::AuthRequest {
+        let AuthRequest { app, app_container, containers } = self;
+
+        let mut containers: Vec<_> = containers.into_iter()
+            .map(|c| c.into_raw())
+            .collect();
+
+        let c_ptr = containers.as_mut_ptr();
+        let c_len = containers.len();
+        let c_cap = containers.capacity();
+
+        mem::forget(containers);
+
+        Box::into_raw(Box::new(ffi::AuthRequest {
+            app: app.into_raw(),
+            app_container: app_container,
+            containers: c_ptr,
+            containers_len: c_len,
+            containers_cap: c_cap,
+        }))
+    }
+
+    /// TODO: doc
+    #[allow(unsafe_code)]
+    pub unsafe fn from_raw(raw: *mut ffi::AuthRequest) -> Self {
+        let raw = Box::from_raw(raw);
+        let app = AppExchangeInfo::from_raw(raw.app);
+        let containers =
+            Vec::from_raw_parts(raw.containers, raw.containers_len, raw.containers_cap)
+                .into_iter()
+                .map(|c| ContainerPermission::from_raw(c))
+                .collect();
+        AuthRequest {
+            app: app,
+            app_container: raw.app_container,
+            containers: containers,
+        }
+    }
 }
 
 /// TODO: doc
@@ -73,6 +225,38 @@ pub struct AppAccessToken {
     pub enc_pk: box_::PublicKey,
     /// TODO: doc
     pub enc_sk: box_::SecretKey,
+}
+
+impl AppAccessToken {
+    /// TODO: doc
+    pub fn into_raw(self) -> *mut ffi::AppAccessToken {
+        let AppAccessToken { enc_key, sign_pk, sign_sk, enc_pk, enc_sk } = self;
+        Box::into_raw(Box::new(ffi::AppAccessToken {
+            enc_key: enc_key.0,
+            sign_pk: sign_pk.0,
+            sign_sk: sign_sk.0,
+            enc_pk: enc_pk.0,
+            enc_sk: enc_sk.0,
+        }))
+    }
+
+    /// TODO: doc
+    #[allow(unsafe_code)]
+    pub unsafe fn from_raw(raw: *mut ffi::AppAccessToken) -> Self {
+        let raw = Box::from_raw(raw);
+        AppAccessToken {
+            enc_key: secretbox::Key::from_slice(&raw.enc_key)
+                .expect("Only fails if struct layout in code is wrong"),
+            sign_pk: sign::PublicKey::from_slice(&raw.sign_pk)
+                .expect("Only fails if struct layout in code is wrong"),
+            sign_sk: sign::SecretKey::from_slice(&raw.sign_sk)
+                .expect("Only fails if struct layout in code is wrong"),
+            enc_pk: box_::PublicKey::from_slice(&raw.enc_pk)
+                .expect("Only fails if struct layout in code is wrong"),
+            enc_sk: box_::SecretKey::from_slice(&raw.enc_sk)
+                .expect("Only fails if struct layout in code is wrong"),
+        }
+    }
 }
 
 /// TODO: doc
