6f9b3a24576f85afca5e5279da7e5ad9c5e2e4cc
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2016-12-07 14:49:14 -0300

    refactor/ipc: Create new `safe_core::ipc::req` module

--- src/ipc/ffi.rs
+++ src/ipc/ffi.rs
@@ -22,116 +22,6 @@
 use rust_sodium::crypto::{box_, secretbox, sign};
 
 #[repr(C)]
-#[derive(Clone, Debug, Eq, PartialEq, RustcEncodable, RustcDecodable)]
-/// The permission type
-pub enum PermissionAccess {
-    /// Read
-    Read,
-    /// Insert
-    Insert,
-    /// Update
-    Update,
-    /// Delete
-    Delete,
-    /// Modify permissions
-    ManagePermissions,
-}
-
-#[repr(C)]
-/// Represents the set of permissions for a given container
-pub struct ContainerPermission {
-    /// The UTF-8 encoded id
-    pub container_key: *const u8,
-    /// `container_key`'s length
-    pub container_key_len: usize,
-    /// Used by the Rust memory allocator
-    pub container_key_cap: usize,
-
-    /// The `PermissionAccess` array
-    pub access: *mut PermissionAccess,
-    /// `access`'s length.
-    pub access_len: usize,
-    /// Used by the Rust memory allocator
-    pub access_cap: usize,
-}
-
-/// Free memory
-#[no_mangle]
-#[allow(unsafe_code)]
-pub unsafe extern "C" fn container_permission_free(cp: *mut ContainerPermission) {
-    let _ = super::ContainerPermission::from_raw(cp);
-}
-
-#[repr(C)]
-/// Represents an application ID in the process of asking permissions
-pub struct AppExchangeInfo {
-    /// UTF-8 encoded id
-    pub id: *const u8,
-    /// `id`'s length
-    pub id_len: usize,
-    /// Used by the Rust memory allocator
-    pub id_cap: usize,
-
-    /// Reserved by the frontend
-    ///
-    /// null if not present
-    pub scope: *const u8,
-    /// `scope`'s length.
-    ///
-    /// 0 if `scope` is null
-    pub scope_len: usize,
-    /// Used by the Rust memory allocator.
-    ///
-    /// 0 if `scope` is null
-    pub scope_cap: usize,
-
-    /// UTF-8 encoded application friendly-name.
-    pub name: *const u8,
-    /// `name`'s length
-    pub name_len: usize,
-    /// Used by the Rust memory allocator
-    pub name_cap: usize,
-
-    /// UTF-8 encoded application provider/vendor (e.g. MaidSafe)
-    pub vendor: *const u8,
-    /// `vendor`'s length
-    pub vendor_len: usize,
-    /// Reserved by the Rust allocator
-    pub vendor_cap: usize,
-}
-
-/// Free memory
-#[no_mangle]
-#[allow(unsafe_code)]
-pub unsafe extern "C" fn app_exchange_info_free(a: *mut AppExchangeInfo) {
-    let _ = super::AppExchangeInfo::from_raw(a);
-}
-
-#[repr(C)]
-/// Represents an authorization request
-pub struct AuthReq {
-    /// The application identifier for this request
-    pub app: *mut AppExchangeInfo,
-    /// `true` if the app wants dedicated container for itself. `false`
-    /// otherwise.
-    pub app_container: bool,
-
-    /// Array of `*mut ContainerPermission`
-    pub containers: *mut *mut ContainerPermission,
-    /// `containers`'s length
-    pub containers_len: usize,
-    /// Reserved by the Rust allocator
-    pub containers_cap: usize,
-}
-
-/// Free memory from the subobjects
-#[no_mangle]
-#[allow(unsafe_code)]
-pub unsafe extern "C" fn auth_request_drop(a: AuthReq) {
-    let _ = super::AuthReq::from_ffi(a);
-}
-
-#[repr(C)]
 /// Represents the needed keys to work with the data
 pub struct AppKeys {
     /// Owner signing public key
@@ -156,9 +46,6 @@ pub struct AppKeys {
 pub unsafe extern "C" fn app_keys_free(a: *mut AppKeys) {
     let _ = super::AppKeys::from_raw(a);
 }
-
-/// Containers request
-pub struct ContainersReq;
 
 /// Containers response
 pub struct ContainersGranted;
